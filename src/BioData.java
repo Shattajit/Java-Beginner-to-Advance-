cdc907132cc02ef21cba91403c1bba5448b0f276
Shattajit <jeet.shatta@gmail.com>
2023-12-30 13:39:00 +0600

    AdvanceJava

--- src/BioData.java
+++ src/BioData.java
@@ -7,7 +7,12 @@ public class BioData {
         Scanner input = new Scanner(System.in);
 
         int val = input.nextInt();
+        double val1 = input.nextDouble();
+        String s = input.nextLine();
 
         System.out.println("the value is:  "+val);
+        System.out.println("the double value is:  "+val1);
+        System.out.println("the string is:  "+s);
+
     }
 }
